16189f8ebf2dda1728b27bfbd205f751f2fd5b64
ignatiev-free <143023616+ignatiev-free@users.noreply.github.com>
2023-11-28 18:01:41 +0300

    Add files via upload

---
+++ PP_2_2_2_spring-mvc-master-master/src/main/java/web/controller/UsersController.java
@@ -0,0 +1,67 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.Model.User;
+import web.service.UserService;
+
+
+@Controller
+
+public class UsersController {
+
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("users", userService.index());
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("userId", userService.show(id));
+        return "id";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.show(id));
+        return "/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") User user, @PathVariable("id") int id) {
+        userService.update(id, user);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/";
+    }
+
+
+}
